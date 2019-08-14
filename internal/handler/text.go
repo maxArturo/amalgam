e8b2535c704fa40ff8883da28620865464892a74
Max Alcala <maxarturo@gmail.com>
2019-08-14 14:50:00 -0500

    change template paths to use globbing

--- internal/handler/text.go
+++ internal/handler/text.go
@@ -4,15 +4,13 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
-	"os"
-	"path/filepath"
 
 	"github.com/maxArturo/amalgam/internal/cache"
 )
 
 func TextHandler(c cache.Cacher) func(w http.ResponseWriter, r *http.Request) {
-	cwd, _ := os.Executable()
-	textTemplate := template.Must(template.ParseFiles(filepath.Join(filepath.Dir(cwd), "config/templates/text.html")))
+	textTemplate := template.Must(template.ParseGlob("config/templates/text.html"))
+
 	return func(w http.ResponseWriter, r *http.Request) {
 		hash := r.URL.Path[len("/text/"):]
 		link, found := c.Get(hash)
